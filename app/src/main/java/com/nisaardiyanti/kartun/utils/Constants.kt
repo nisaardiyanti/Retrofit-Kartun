c1887fdecc5a79a3eef7c1b47a3e6716732d2ccc
nisaardiyanti <100394858+nisaardiyanti@users.noreply.github.com>
2022-11-17 11:29:11 +0700

    Update Constants.kt

--- app/src/main/java/com/nisaardiyanti/kartun/utils/Constants.kt
+++ app/src/main/java/com/nisaardiyanti/kartun/utils/Constants.kt
@@ -3,6 +3,7 @@ package com.nisaardiyanti.kartun.utils
 object Constants {
 
     const val BASE_URL: String = "https://rickandmortyapi.com/api/"
+    //No Tujuh url gambar
     //Url yang digunakan untuk menyambungkan data yang akan ditampilkan kedalam aplikasi
     // yang diambil dari internet
     const val END_POINT: String = "character"
